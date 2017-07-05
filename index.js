bfb6388886f1da4f1dfd2e71593cfbbd2bdf07dd
Rachael Coulter <rcoulter700@googlemail.com>
2017-07-05 09:56:02 +0100

    Bot basic setup

---
+++ index.js
@@ -0,0 +1,43 @@
+// dependencies
+
+var restify = require('restify');
+
+var builder = require('botbuilder');
+
+// Setup Restify Server
+
+var server = restify.createServer();
+
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+
+   console.log('%s listening to %s', server.name, server.url); 
+
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+
+var connector = new builder.ChatConnector({
+
+    appId: process.env.MICROSOFT_APP_ID,
+
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+
+});
+
+// create the bot
+
+var bot = new builder.UniversalBot(connector);
+
+// Listen for messages from users 
+
+server.post('/bot', connector.listen());
+
+// This bot enables users to either make a dinner reservation or order dinner.
+
+var bot = new builder.UniversalBot(connector, function(session){
+
+    var msg = "Welcome to the reservation bot. Please say `Dinner Reservation` or `Order Dinner`";
+
+    session.send(msg);
+
+});
